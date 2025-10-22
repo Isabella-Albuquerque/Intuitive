6da9d2ac003e4517b558fd551a832e1fa54904cf
Katia Martins <katiamartins13@yahoo.com.br>
2025-10-22 20:28:14 -0300

    fix: corrige implementacao de emocao antes e CORS

--- backend/src/main/java/com/intuitive/app/controller/RelatoriosController.java
+++ backend/src/main/java/com/intuitive/app/controller/RelatoriosController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Arrays;
 import java.util.List;
 
+@CrossOrigin(origins = "*") 
 @RestController
 @RequestMapping("/relatorios")
 public class RelatoriosController {
